cde8ebecd27913e0edec63ae87d1f1b649943442
Aleksandr Shamaraev <153871261+AlexanderShad@users.noreply.github.com>
2025-03-21 15:36:59 +1100

    v.0.3

--- msgextract.sh
+++ msgextract.sh
@@ -1,48 +1,73 @@
 #!/bin/bash
-# Easily extract the contents of MSG email files v.0.2
+# Easily extract the contents of MSG email files v.0.3
 # https://github.com/AlexanderShad/msgextract
 
-echo -e '\e[042;30m --- Start extraction... --- \033[0m\n'
+if [[ -n "$1" ]]; then
+	while [[ $# -gt 0 ]]; do
+		case "$1" in
 
-if [ -n "$1" ] ; then
+			--help|-h)
+				echo -e '\e[042;30m --- Easily extract the contents of MSG email files --- \033[0m\n'
+				echo ""
+				echo "msgextract <file name>.msg"
+				echo ""
+				echo "--help, -h - show help"
+				echo "--version, -v - show version"
+				echo ""
+				exit 0
+			;;
 
-	if [ -e "$(cd "$(dirname "$1")"; pwd -P)/$(basename "$1")" ] ; then
+			--version|-v)
+				echo "version 0.3"
+				exit 0
+			;;
 
-		echo ''
-		echo -e '\e[0;31m* creating temp file...\e[0m'
-		echo ''
+			*)
+				if [[ $1 =~ ".msg" ]]; then
 
-		cp "$(cd "$(dirname "$1")"; pwd -P)/$(basename "$1")" "$(cd "$(dirname "$1")"; pwd -P)/tfile.msg"
+					echo -e '\e[042;30m --- Start extraction... --- \033[0m\n'
 
-		echo ''
-		echo -e '\e[0;31m* msgconverting...\e[0m'
-		echo ''
-		
-	 	msgconvert "$(cd "$(dirname "$1")"; pwd -P)/tfile.msg"
-	 	
-	 	echo ''
-		echo -e '\e[0;31m* munpacking...\e[0m'
-		echo ''
- 		munpack "$(cd "$(dirname "$1")"; pwd -P)/tfile.eml"
+					if [ -e "$(cd "$(dirname "$1")"; pwd -P)/$(basename "$1")" ] ; then
 
-		echo ''
-		echo -e '\e[0;31m* deleting temp files...\e[0m'
-		echo ''
-	 	rm "$(cd "$(dirname "$1")"; pwd -P)/tfile.eml"
- 		rm "$(cd "$(dirname "$1")"; pwd -P)/tfile.msg"
+						echo ''
+						echo -e '\e[0;31m* creating temp file...\e[0m'
+						echo ''
 
-	 	echo -e '\e[042;30m --- Extracted --- \033[0m\n'
+						cp "$(cd "$(dirname "$1")"; pwd -P)/$(basename "$1")" "$(cd "$(dirname "$1")"; pwd -P)/tfile.msg"
 
-	else
- 		echo -e '\e[042;30m --- File not found. --- \033[0m\n'	
- 	fi
- 	
+						echo ''
+						echo -e '\e[0;31m* msgconverting...\e[0m'
+						echo ''
+
+						msgconvert "$(cd "$(dirname "$1")"; pwd -P)/tfile.msg"
+
+						echo ''
+						echo -e '\e[0;31m* munpacking...\e[0m'
+						echo ''
+
+						munpack "$(cd "$(dirname "$1")"; pwd -P)/tfile.eml"
+
+						echo ''
+						echo -e '\e[0;31m* deleting temp files...\e[0m'
+						echo ''
+
+						rm "$(cd "$(dirname "$1")"; pwd -P)/tfile.eml"
+						rm "$(cd "$(dirname "$1")"; pwd -P)/tfile.msg"
+
+						echo -e '\e[042;30m --- Extracted --- \033[0m\n'
+
+					else
+						echo -e '\e[042;30m --- File not found. --- \033[0m\n'
+					fi
+					exit 0
+				else
+					echo -e '\e[042;30m --- Unknown argument. --- \033[0m\n'
+					exit 0
+				fi
+			;;
+
+		esac
+	done
 else
-
- echo -e '\e[042;30m --- No parameters found. --- \033[0m\n'
- 
+	echo -e '\e[042;30m --- No parameters found. --- \033[0m\n'
 fi
-
-
-
-
